77b75fe1698d9c32d20bd9164c532c31d1acbe38
littlesluggerboi <tayaotaoaeron@gmail.com>
2025-04-09 02:39:55 +0800

    Added error pages designs.

--- validators/validator.js
+++ validators/validator.js
@@ -3,7 +3,7 @@ import { validationResult } from "express-validator"
 const validator = async (req, res, next) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
-        return res.status(400).json(errors);
+        return res.status(400).render("/error_pages/404.ejs");
     }
     next();
 }
