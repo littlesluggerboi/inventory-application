cdf20ec0dbb3bcacf59f4fa48a03f68f4ba7c45b
littlesluggerboi <tayaotaoaeron@gmail.com>
2025-04-09 09:20:07 +0800

    Added modules and app index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import express from "express";
 import path from "node:path";
+import gameRouter from "./routes/game_route.js";
+import genreRouter from "./routes/genre_route.js";
+import indexRouter from "./routes/index_route.js";
+import devsRouter from "./routes/devs_route.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.set("views", path.join(import.meta.dirname, "views"));
@@ -7,16 +13,20 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(import.meta.dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
-app.use((err, req, res, next)=>{
-    console.log(err.message);
-    res.status(500).send("Error 500 | Internal Error");
-  })
-  
-  app.use((req, res)=>{
-      res.status(404).send("Error 404 | Not Found")
-  })
-  
-  
-  app.listen(8080, () => {
-    console.log(`listening at port: 8080`);
-  });
+
+app.use("/", indexRouter);
+app.use("/games", gameRouter);
+app.use("/genres", genreRouter);
+app.use("/developers", devsRouter);
+app.use((err, req, res, next) => {
+  console.log(err.message);
+  res.status(500).render("error_pages/500.ejs");
+});
+
+app.use((req, res) => {
+  res.status(404).render("error_pages/404.ejs");
+});
+
+app.listen(8080, () => {
+  console.log(`listening at port: 8080`);
+});
